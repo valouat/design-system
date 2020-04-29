9db56bf7058ddf8488789c6b4e005dc246a12855
Val <valentinedestrait@hotmail.com>
2020-04-29 15:18:47 +0200

    add avatar test

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Button } from "@storybook/react/demo";
+import { Avatar } from "../Avatar";
 
 export default {
-  title: 'Button',
+  title: "Button",
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => (
+  <Button onClick={action("clicked")}>Hello Button</Button>
+);
 
 export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
+  <>
+    <Button onClick={action("clicked")}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+    <Avatar username="Valentine Dort" />
+  </>
 );
